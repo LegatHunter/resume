c79b8233d66f44f2cc7ef3be2a132bec0934bcce
Nikita Prebyshevskiy <legathunter@mail.ru>
2025-03-20 22:50:28 +0300

    Upgrade

--- script/app.js
+++ script/app.js
@@ -15,7 +15,6 @@ const App = {
                 urlLinkedin: 'https://www.linkedin.com/in/%D0%BD%D0%B8%D0%BA%D0%B8%D1%82%D0%B0-%D0%BF%D1%80%D0%B5%D0%B1%D1%8B%D1%88%D0%B5%D0%B2%D1%81%D0%BA%D0%B8%D0%B9-50b54984',
                 urlMailto: 'mailto:i@legathunter.ru?subject=Привет&body=Мы%20хотим%20предложить%20тебе%20очень%20высокооплачиваемую%20работу!)))',
             },
-
         };
     },
 }
